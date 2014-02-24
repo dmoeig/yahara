6bc1e8f7a13a02725672e3194d8030979236d706
Steve Faulkner <southpolesteve@gmail.com>
2014-02-24 13:42:30 -0600

    Remove old tmp stuff that was left in the repo

--- tmp/javascript/app/routes/albums.js
+++
@@ -1,7 +0,0 @@
-import Album from "appkit/models/album";
-
-export default Ember.Route.extend({
-  model: function() {
-    return Album.fetch();
-  }
-});
--- tmp/transpiled/app/controllers/application.js
+++
@@ -1,9 +0,0 @@
-define("appkit/controllers/application", 
-  ["exports"],
-  function(__exports__) {
-    "use strict";
-    __exports__["default"] = Ember.ObjectController.extend({
-      token: null,
-      cardNumber: null
-    });
-  });
