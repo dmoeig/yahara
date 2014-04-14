ecbc13018f85116714fa08910ea33058af5b41db
Steve Faulkner <southpolesteve@gmail.com>
2014-04-13 21:55:06 -0500

    Adapter cleanup

--- app/adapters/yahara.js
+++ app/adapters/yahara.js
@@ -41,70 +41,6 @@ Yahara.Adapter = Ember.Adapter.extend({
     records.load(klass, dataToLoad);
   },
 
-  findQuery: function(klass, records, params) {
-    var url = this.buildURL(klass),
-        self = this;
-
-    return this.ajax(url, params).then(function(data) {
-      self.didFindQuery(klass, records, params, data);
-      return records;
-    });
-  },
-
-  didFindQuery: function(klass, records, params, data) {
-      var collectionKey = Ember.get(klass, 'collectionKey'),
-          dataToLoad = collectionKey ? data[collectionKey] : data;
-
-      records.load(klass, dataToLoad);
-  },
-
-  createRecord: function(record) {
-    var url = this.buildURL(record.constructor),
-        self = this;
-
-    return this.ajax(url, record.toJSON(), "POST").then(function(data) {
-      self.didCreateRecord(record, data);
-      return record;
-    });
-  },
-
-  didCreateRecord: function(record, data) {
-    var rootKey = Ember.get(record.constructor, 'rootKey'),
-        primaryKey = Ember.get(record.constructor, 'primaryKey'),
-        dataToLoad = rootKey ? data[rootKey] : data;
-    record.load(dataToLoad[primaryKey], dataToLoad);
-    record.didCreateRecord();
-  },
-
-  saveRecord: function(record) {
-    var primaryKey = Ember.get(record.constructor, 'primaryKey'),
-        url = this.buildURL(record.constructor, Ember.get(record, primaryKey)),
-        self = this;
-
-    return this.ajax(url, record.toJSON(), "PUT").then(function(data) {  // TODO: Some APIs may or may not return data
-      self.didSaveRecord(record, data);
-      return record;
-    });
-  },
-
-  didSaveRecord: function(record, data) {
-    record.didSaveRecord();
-  },
-
-  deleteRecord: function(record) {
-    var primaryKey = Ember.get(record.constructor, 'primaryKey'),
-        url = this.buildURL(record.constructor, Ember.get(record, primaryKey)),
-        self = this;
-
-    return this.ajax(url, record.toJSON(), "DELETE").then(function(data) {  // TODO: Some APIs may or may not return data
-      self.didDeleteRecord(record, data);
-    });
-  },
-
-  didDeleteRecord: function(record, data) {
-    record.didDeleteRecord();
-  },
-
   ajax: function(url, params, method, settings) {
     return this._ajax(url, params, (method || "GET"), settings);
   },
@@ -121,43 +57,10 @@ Yahara.Adapter = Ember.Adapter.extend({
     }
   },
 
-  ajaxSettings: function(url, method) {
-    return {
-      url: url,
-      type: method
-    };
-  },
-
   _ajax: function(url, params, method, settings) {
-    if (!settings) {
-      settings = this.ajaxSettings(url, method);
-    }
-
-    return new Ember.RSVP.Promise(function(resolve, reject) {
-      if (params) {
-        if (method === "GET") {
-          settings.data = params;
-        } else {
-          settings.contentType = "application/json; charset=utf-8";
-          settings.data = JSON.stringify(params);
-        }
-      }
-
-      settings.success = function(json) {
-        Ember.run(null, resolve, json);
-      };
-
-      settings.error = function(jqXHR, textStatus, errorThrown) {
-        // https://github.com/ebryn/ember-model/issues/202
-        if (jqXHR) {
-          jqXHR.then = null;
-        }
-
-        Ember.run(null, reject, jqXHR);
-      };
-
-
-      Ember.$.ajax(settings);
-   });
+    return ic.ajax.request({
+      url: url,
+      method: method
+    });
   }
 });
