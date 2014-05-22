58d1aceceae3de87abcd48b1c677248c5a55024f
Steve Faulkner <southpolesteve@gmail.com>
2014-05-22 16:17:51 -0500

    Remove Old MARC

--- marc/marc.js
+++ marc/marc.js
@@ -1,29 +1,37 @@
 var request = require('request');
 var fs = require('fs');
 var marc = require('marcjs');
+var xmlfile = fs.createWriteStream('records.xml')
+var txtfile = fs.createWriteStream('records.txt')
+
+var writers = [
+    new marc.MarcxmlWriter(xmlfile),
+    new marc.TextWriter(txtfile)
+];
 
 request('https://yahara-api.herokuapp.com/catalog/yahara', function (error, response, body) {
   JSON.parse(body).forEach(function(album){
     var record = new marc.Record();
-    var writers = [
-        new marc.MarcxmlWriter(fs.createWriteStream(album.slug + '.xml')),
-        new marc.Iso2709Writer(fs.createWriteStream(album.slug + '.mrc')),
-        new marc.JsonWriter(fs.createWriteStream(album.slug + '.json')),
-        new marc.TextWriter(fs.createWriteStream(album.slug + '.txt'))
-    ];
-    record.append(['006','m q h'])
-    record.append(['007','sz zunnnnnzneu'])
-    record.append(['250','a','[Downloadable music]'])
-    record.append(['500', ' ', 'a', 'Downloadable audio file.']);
-    record.append(['500', ' ', 'a', 'Title from title details screen.']);
+
+    record.append(['006', '  ', 'm q h'])
+    record.append(['007', '  ', 'sz zunnnnnzneu'])
+    record.append(['100', '1 ', 'a', album.artist_name])
+    record.append(['245', '10', 'a', album.title, 'c', album.artist_name ])
+    record.append(['250', '  ', 'a','[Downloadable music]'])
+    record.append(['260', '  ', 'b', album.record_label, 'c', album.release_date]);
+    record.append(['500', '  ', 'a', 'Downloadable audio file.']);
+    record.append(['500', '  ', 'a', 'Title from digitaly encoded metadata.']);
+    record.append(['538', '  ', 'a', 'Mode of access: World Wide Web.']);
+    record.append(['856', '40', 'u', 'http://www.yaharamusic.org/album/'+album.slug]);
 
     var tracks = ['505', '00']
     album.tracks.forEach(function(track){
-      tracks = tracks.concat(['t', track.title])
+      tracks = tracks.concat(['t', track.title +' -- '])
     })
     record.append(tracks);
 
     writers.forEach(function(writer) { writer.write(record);} );
-    writers.forEach(function(writer) { writer.end();} );
   })
 })
+
+writers.forEach(function(writer) { writer.end();} );
